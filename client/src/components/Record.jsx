052cd55a8833a205c5c67a9cfd97b999ef613ddb
Anes075 <anes.brkic@gritacademy.se>
2025-09-04 09:32:11 +0200

    Updated front

--- client/src/components/Record.jsx
+++ client/src/components/Record.jsx
@@ -154,7 +154,7 @@ export default function Record() {
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
-                    type="number"
+                    type="string"
                     name="unit"
                     id="unit"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
@@ -164,11 +164,71 @@ export default function Record() {
                 </div>
               </div>
             </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="packageSize"
+                className="block text-sm font-medium leading-6 text-slate-900"
+              >
+                Package Size
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="number"
+                    name="packageSize"
+                    id="packageSize"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    value={form.packageSize}
+                    onChange={(e) => updateForm({ packageSize: e.target.value })}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="purchasePrice"
+                className="block text-sm font-medium leading-6 text-slate-900"
+              >
+                Purchase Price
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="number"
+                    name="purchasePrice"
+                    id="purchasePrice"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    value={form.purchasePrice}
+                    onChange={(e) => updateForm({ purchasePrice: e.target.value })}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="salesPrice"
+                className="block text-sm font-medium leading-6 text-slate-900"
+              >
+                Sales Price
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="number"
+                    name="salesPrice"
+                    id="salesPrice"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    value={form.salesPrice}
+                    onChange={(e) => updateForm({ salesPrice: e.target.value })}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <input
           type="submit"
-          value="Save Employee Record"
+          value="Save Article"
           className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
         />
       </form>
